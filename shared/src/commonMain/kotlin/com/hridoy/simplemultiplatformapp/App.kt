0054c9e62b444c43294c2cd473999ca4b0e45a3c
Hridoy Chandra Das <32935576+ihridoydas@users.noreply.github.com>
2023-06-10 12:34:09 +0900

    Basic set IOs and Android

--- shared/src/commonMain/kotlin/com/hridoy/simplemultiplatformapp/App.kt
+++ shared/src/commonMain/kotlin/com/hridoy/simplemultiplatformapp/App.kt
@@ -1,33 +1,31 @@
 package com.hridoy.simplemultiplatformapp
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 
 @Composable
 fun App() {
-    var count by remember {
-        mutableStateOf(0)
-    }
+
 
     Box(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier.fillMaxSize()
+            .background(Color.White),
         contentAlignment = Alignment.Center
     ){
 
-        Button(onClick = {
-            count++
-        }){
-            Text("Count $count")
-        }
+        Text(
+            modifier = Modifier,
+            style = TextStyle(textAlign = TextAlign.Center),
+            text = "Hello World \nwith Kotlin MultiplatForm"
+        )
 
     }
 }
